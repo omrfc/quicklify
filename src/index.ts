d3482369aeac0b8e5dc66945e5b3f856df25c26a
omrfccc-png <omrfccc@gmail.com>
2026-02-14 13:04:51 +0300

    fix: read version from package.json dynamically

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
 #!/usr/bin/env node
 
 import { Command } from "commander";
+import { readFileSync } from "fs";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 import { initCommand } from "./commands/init.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const pkg = JSON.parse(readFileSync(join(__dirname, "../package.json"), "utf-8"));
 
 const program = new Command();
 
 program
   .name("quicklify")
   .description("Automate Coolify deployment on cloud providers")
-  .version("0.1.0");
+  .version(pkg.version);
 
 program
   .command("init")
